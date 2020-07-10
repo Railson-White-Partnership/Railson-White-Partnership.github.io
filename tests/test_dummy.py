5822094fa266559cf67242cc434bf1590460ba56
Daniel Brownridge <daniel@freshnewpage.com>
2020-07-10 08:11:30 +0100

    [#2] improve test document wording

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -4,4 +4,4 @@ Trivial 'smoke-tests' to check the testing framework is working.
 """
 
 def test_pass() -> None:
-    """Test intended to always. Passes by default because empty."""
+    """Test intended to always pass. Passes by default because empty."""
